5f4ec660aa06f0457509a2392b71cea119f584d1
João Victor <ruasjoaovictorsantos@gmail.com>
2024-02-09 10:18:02 -0300

    Inicio da produção da pagina inicial

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" type="image/png" href="https://media.discordapp.net/attachments/820976935077281843/1202681737000263690/Nova_Logo_RigRover.png?ex=65d79226&is=65c51d26&hm=03fa13efe2d426d96df024a835121470c2babce5cdf76392ca41e5b0aba54a8b&=&format=webp&quality=lossless&width=450&height=599">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
+    <title>Página Inicial - RigRover</title>
+</head>
+<body>
+    <nav class="navbar">
+    
+    <div>
+        <img src="img\Nova_Logo_RigRover.png" alt="" class="logonav">
+    </div>
+
+        <ul>
+            <li>
+                <a href="#">Página Inicial</a>
+            </li>
+            <li>
+                <a href="#">Quem Somos?</a>
+            </li>
+            <li>
+                <a href="#">Noticias</a>
+            </li>
+            <li>
+                <a href="#">Comprar Hardware</a>
+            </li>
+        </ul>
+    </nav>
+
+
+    <div class="botoes">
+        <button>
+            <a href="cadastro.php">Cadastrar-se</a>
+        </button>
+
+        <button>
+            <a href="login.php">Entrar</a>
+        </button>
+    </div>
+</body>
+</html>
