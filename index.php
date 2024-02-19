c07b3f25be000af0f9a0a3c497c1fa112121bda2
João Victor <ruasjoaovictorsantos@gmail.com>
2024-02-19 10:01:23 -0300

    pré-finalização do css do index, inicialização do css do cadastro.

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    <link rel="icon" type="image/png" href="https://media.discordapp.net/attachments/820976935077281843/1202681737000263690/Nova_Logo_RigRover.png?ex=65d79226&is=65c51d26&hm=03fa13efe2d426d96df024a835121470c2babce5cdf76392ca41e5b0aba54a8b&=&format=webp&quality=lossless&width=450&height=599">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" type="image/png" href="https://media.discordapp.net/attachments/1097493454545764472/1207911558559170601/Nova_Logo_RigRover_1.png?ex=65e15e4d&is=65cee94d&hm=1f91b588742806550adf33275c874fa2149d9f66260d040373d4f8d2c32371e8&=&format=webp&quality=lossless&width=450&height=600">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
@@ -33,15 +33,16 @@
         </ul>
     </nav>
 
+    <div class="centro">
+    <img src="img\logo2.png" alt=""class="logodois">
+    <p class="slogan">Equipando Sua Jornada,</p> 
+    <p class="slogan">Elevando Seu Jogo.</p>
+    <div>
+            <a href="cadastro.php"><button  class="botoes">Cadastrar-se</button></a>
 
-    <div class="botoes">
-        <button>
-            <a href="cadastro.php">Cadastrar-se</a>
-        </button>
-
-        <button>
-            <a href="login.php">Entrar</a>
-        </button>
+            <a href="login.php"> <button  class="botoes">Entrar</button></a>
+        
+    </div>
     </div>
 </body>
 </html>
