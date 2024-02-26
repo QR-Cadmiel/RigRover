afc5a7acc4f113303a81f5d2265b17b1e0df03e7
Pedro Gabriel (fã do manoel gomes) <pedrogabrielxx268@gmail.com>
2024-02-26 11:17:16 -0300

    Atualização do index.

--- index.php
+++ index.php
@@ -1,48 +1,178 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">
-    <link rel="icon" type="image/png" href="https://media.discordapp.net/attachments/1097493454545764472/1207911558559170601/Nova_Logo_RigRover_1.png?ex=65e15e4d&is=65cee94d&hm=1f91b588742806550adf33275c874fa2149d9f66260d040373d4f8d2c32371e8&=&format=webp&quality=lossless&width=450&height=600">
+    <link rel="icon" type="image/png"
+        href="https://media.discordapp.net/attachments/1097493454545764472/1207911558559170601/Nova_Logo_RigRover_1.png?ex=65e15e4d&is=65cee94d&hm=1f91b588742806550adf33275c874fa2149d9f66260d040373d4f8d2c32371e8&=&format=webp&quality=lossless&width=450&height=600">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans&display=swap" rel="stylesheet">
     <title>Página Inicial - RigRover</title>
 </head>
+
 <body>
-    <nav class="navbar">
-    
-    <div>
-        <img src="img\Nova_Logo_RigRover.png" alt="" class="logonav">
+    <div class="rigrover-1">
+        <nav class="navbar">
+
+            <div>
+                <img src="img\Nova_Logo_RigRover.png" alt="" class="logonav">
+            </div>
+
+            <ul>
+                <li>
+                    <a href="#">Página Inicial</a>
+                </li>
+                <li>
+                    <a href="#">Quem Somos?</a>
+                </li>
+                <li>
+                    <a href="#">Noticias</a>
+                </li>
+                <li>
+                    <a href="#">Comprar Hardware</a>
+                </li>
+            </ul>
+        </nav>
+
+        <div class="centro">
+            <img src="img\logo2.png" alt="" class="logodois">
+            <p class="slogan">Equipando Sua Jornada,</p>
+            <p class="slogan">Elevando Seu Jogo.</p>
+            <div>
+                <a href="cadastro.php"><button class="botoes">Cadastrar-se</button></a>
+
+                <a href="login.php"> <button class="botoes">Entrar</button></a>
+
+            </div>
+        </div>
     </div>
 
-        <ul>
-            <li>
-                <a href="#">Página Inicial</a>
-            </li>
-            <li>
-                <a href="#">Quem Somos?</a>
-            </li>
-            <li>
-                <a href="#">Noticias</a>
-            </li>
-            <li>
-                <a href="#">Comprar Hardware</a>
-            </li>
-        </ul>
-    </nav>
+    <div id=rigrover-2>
+        <div class=i1t2>
+            <img src="img/camaleao.jpg" alt="" width="600px">
+            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae nulla enim. Nulla scelerisque arcu
+                sapien. Quisque ac convallis ante. In ac tincidunt nisi. Integer a consectetur ante. Quisque non iaculis
+                orci. In hac habitasse platea dictumst. Cras aliquet pretium tortor ut sodales. Nullam iaculis non lacus
+                et volutpat. Duis sit amet lacus vel justo tincidunt venenatis. Sed ultricies ante sagittis, mattis ante
+                vitae, lobortis purus. Mauris vehicula, ligula eu cursus ullamcorper, metus metus luctus eros, a commodo
+                dolor sem non diam. Integer porttitor varius sem eget molestie. Fusce vel eleifend neque. Curabitur sit
+                amet rutrum lectus.</h5>
+            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae nulla enim. Nulla scelerisque arcu
+                sapien. Quisque ac convallis ante. In ac tincidunt nisi. Integer a consectetur ante. Quisque non iaculis
+                orci. In hac habitasse platea dictumst. Cras aliquet pretium tortor ut sodales. Nullam iaculis non lacus
+                et volutpat. Duis sit amet lacus vel justo tincidunt venenatis. Sed ultricies ante sagittis, mattis ante
+                vitae, lobortis purus. Mauris vehicula, ligula eu cursus ullamcorper, metus metus luctus eros, a commodo
+                dolor sem non diam. Integer porttitor varius sem eget molestie. Fusce vel eleifend neque. Curabitur sit
+                amet rutrum lectus.</h5>
+            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae nulla enim. Nulla scelerisque arcu
+                sapien. Quisque ac convallis ante. In ac tincidunt nisi. Integer a consectetur ante. Quisque non iaculis
+                orci. In hac habitasse platea dictumst. Cras aliquet pretium tortor ut sodales. Nullam iaculis non lacus
+                et volutpat. Duis sit amet lacus vel justo tincidunt venenatis. Sed ultricies ante sagittis, mattis ante
+                vitae, lobortis purus. Mauris vehicula, ligula eu cursus ullamcorper, metus metus luctus eros, a commodo
+                dolor sem non diam. Integer porttitor varius sem eget molestie. Fusce vel eleifend neque. Curabitur sit
+                amet rutrum lectus.</h5>
+        </div>
 
-    <div class="centro">
-    <img src="img\logo2.png" alt=""class="logodois">
-    <p class="slogan">Equipando Sua Jornada,</p> 
-    <p class="slogan">Elevando Seu Jogo.</p>
-    <div>
-            <a href="cadastro.php"><button  class="botoes">Cadastrar-se</button></a>
+        <div class=i2t1>
+            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae nulla enim. Nulla scelerisque arcu
+                sapien. Quisque ac convallis ante. In ac tincidunt nisi. Integer a consectetur ante. Quisque non iaculis
+                orci. In hac habitasse platea dictumst. Cras aliquet pretium tortor ut sodales. Nullam iaculis non lacus
+                et volutpat. Duis sit amet lacus vel justo tincidunt venenatis. Sed ultricies ante sagittis, mattis ante
+                vitae, lobortis purus. Mauris vehicula, ligula eu cursus ullamcorper, metus metus luctus eros, a commodo
+                dolor sem non diam. Integer porttitor varius sem eget molestie. Fusce vel eleifend neque. Curabitur sit
+                amet rutrum lectus.</h5>
+            <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vitae nulla enim. Nulla scelerisque arcu
+                sapien. Quisque ac convallis ante. In ac tincidunt nisi. Integer a consectetur ante. Quisque non iaculis
+                orci. In hac habitasse platea dictumst. Cras aliquet pretium tortor ut sodales. Nullam iaculis non lacus
+                et volutpat. Duis sit amet lacus vel justo tincidunt venenatis. Sed ultricies ante sagittis, mattis ante
+                vitae, lobortis purus. Mauris vehicula, ligula eu cursus ullamcorper, metus metus luctus eros, a commodo
+                dolor sem non diam. Integer porttitor varius sem eget molestie. Fusce vel eleifend neque. Curabitur sit
+                amet rutrum lectus.</h5>
+            <img src="img/lagarto.jpg" alt="" width="600px" height="400px">
+        </div>
+    </div>
 
-            <a href="login.php"> <button  class="botoes">Entrar</button></a>
-        
-    </div>
-    </div>
+    <div id=rigrover-3>
+        <div class=title-nav>
+            <h1>Equipe Desenvolvedora</h1>
+            <img src="img/empresa.png" alt="" width="200px" height>
+        </div>
+
+        <div class="pictures-container">
+            <div class="pictures-1">
+                <div class="avatar">
+                    <div class="cadmiel">
+                        <img src="img/cadmiel.png" alt="" width="200px">
+                    </div>
+                    <div>
+                        <h2>Cadmiel Torres</h2>
+                        <h3>Desenvolvedor</h3>
+                    </div>
+                </div>
+                <div class="avatar">
+                    <div class="pedro">
+                        <img src="img/pedro.png" alt="" width="200px">
+                    </div>
+                    <div>
+                        <h2>Pedro Gabriel</h2>
+                        <h3>Project Owner</h3>
+                    </div>
+                </div>
+            </div>
+
+            <div class="pictures-2">
+                <div class="avatar">
+                    <div class="joao">
+                        <img src="img/joao.jpg" alt="" width="200px">
+                    </div>
+
+                    <div>
+                        <h2>João Victor</h2>
+                        <h3>Desenvolvedor</h3>
+                    </div>
+                </div>
+                <div class="avatar">
+                    <div class="thiago">
+                        <img src="img/thiago.webp" alt="" width="200px">
+                    </div>
+                    <div>
+                        <h2>Thiago Felipe</h2>
+                        <h3>Desenvolvedor</h3>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div id="rigrover-4">
+            <div class=top-pg>
+                <h1>Perguntas frequentes</h1>
+            </div>
+
+            <button class="btn-question">As noticias são confiáveis?<img
+                    src="https://www.pngmart.com/files/15/Vector-Arrow-Down-PNG-Image.png" alt="" widht="30px"
+                    height="30px"></button>
+            <button class="btn-question">A ferramenta de analisar hardware, funciona?<img
+                    src="https://www.pngmart.com/files/15/Vector-Arrow-Down-PNG-Image.png" alt="" widht="30px"
+                    height="30px"></button>
+            <button class="btn-question">A ferramenta de analisar hardware, funciona?<img
+                    src="https://www.pngmart.com/files/15/Vector-Arrow-Down-PNG-Image.png" alt="" widht="30px"
+                    height="30px"></button>
+            <button class="btn-question">A ferramenta de analisar hardware, funciona?<img
+                    src="https://www.pngmart.com/files/15/Vector-Arrow-Down-PNG-Image.png" alt="" widht="30px"
+                    height="30px"></button>
+
+        </div>
+
+        <footer>
+            <div class="footer-content">
+                <p>Footer</p>
+            </div>
+
+        </footer>
+
 </body>
+
 </html>
